e546ce1f3bc3ea0083f4606ca5177ad90bd03e10
871246995 <871246995@qq.com>
2018-04-16 17:08:03 +0800

    ok

--- app/routes/wx.js
+++ app/routes/wx.js
@@ -13,14 +13,15 @@ router.post('/', function(req, res, next) {
   console.log(req.params,':params');
   console.log(req.query,':query');
   var toUser = req.query.openid;
+  var fromUserName = 'IVANLEE99-LLFFOREVER'
+  var time = new Date().getTime();
 
 var s = `<xml>  
 <ToUserName>< ![CDATA[${toUser}] ]></ToUserName>  
-<FromUserName>< ![CDATA[${toUser}] ]></FromUserName>  
-<CreateTime>1348831860</CreateTime>  
+<FromUserName>< ![CDATA[${fromUserName}] ]></FromUserName>  
+<CreateTime>${time}</CreateTime>  
 <MsgType>< ![CDATA[text] ]></MsgType>  
-<Content>< ![CDATA[this is a test] ]></Content>  
-<MsgId>1234567890123456</MsgId>  
+<Content>< ![CDATA[你好！] ]></Content> 
 </xml>`
   res.send(s);
 });
