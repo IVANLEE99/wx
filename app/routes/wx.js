306312642963bf8a8faf1488c546640ebdd9217f
871246995 <871246995@qq.com>
2018-04-16 22:53:46 +0800

    被动回复消息

--- app/routes/wx.js
+++ app/routes/wx.js
@@ -8,13 +8,42 @@ router.get('/', function(req, res, next) {
 });
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  console.log(req.body,':body');
+  // console.log(req.body,':body');
+  // { xml:
+  //   { tousername: [ 'gh_97296cad7184' ],
+  //     fromusername: [ 'o7i2V1PDjnUCRpPssALMi2caVsuI' ],
+  //     createtime: [ '1523889693' ],
+  //     msgtype: [ 'text' ],
+  //     content: [ '你是最棒的！' ],
+  //     msgid: [ '6545056394573526012' ] } } ':body'
   console.log(req.params,':params');
   console.log(req.query,':query');
   var toUser = req.query.openid;
   var fromUserName = 'IVANLEE99-LLFFOREVER'
   var time = new Date().getTime();
-  res.send('');
+  var data = {
+    fromusername:req.body.xml.fromusername,
+    tousername:req.body.xml.tousername,
+    content:'终于等到你,还好我没放弃!'
+  }
+  msgText(res,data)
 });
+
+function msgText(res,data){
+  //fromusername 接受者openid
+  //tousername  发送者openid
+  res.set('Content-Type', 'application/xml');
+  res.status(200);
+   var resMsg = '<xml>' +
+      '<ToUserName><![CDATA[' + data.fromusername + ']]></ToUserName>' +
+      '<FromUserName><![CDATA[' + data.tousername + ']]></FromUserName>' +
+      '<CreateTime>' + parseInt(new Date().valueOf() / 1000) + '</CreateTime>' +
+      '<MsgType><![CDATA[text]]></MsgType>' +
+      '<Content><![CDATA['+data.content+']]></Content>' +
+      '</xml>';
+    res.end(resMsg);
+     res.send(resMsg);
+    
+}
 
 module.exports = router;
